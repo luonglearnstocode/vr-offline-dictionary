a768216833d2143ff58e5b766b0fc2579e951281
luonglearnstocode <luong.nguyen@metropolia.fi>
2018-10-27 23:59:17 +0300

    add script import json to firestore

---
+++ importToFirestore.js
@@ -0,0 +1,20 @@
+var firebase = require("firebase");
+const dict = require('./input.json')
+
+var config = {
+  apiKey: "AIzaSyAvDep8RmCoR8xabSQRXY6_LGa3BHBiPfU",
+  authDomain: "vr-dict.firebaseapp.com",
+  databaseURL: "https://vr-dict.firebaseio.com",
+  projectId: "vr-dict",
+  storageBucket: "vr-dict.appspot.com",
+  messagingSenderId: "971156516516"
+};
+firebase.initializeApp(config);
+const db = firebase.firestore()
+db.settings({ timestampsInSnapshots: true });
+
+dict.forEach(word => {
+  db.collection('words').add(word)
+});
+return console.log('done');
+
