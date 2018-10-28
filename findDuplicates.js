277ecb20723f6efae16f86bd3fe47fba7f702f31
luonglearnstocode <luong.nguyen@metropolia.fi>
2018-10-28 03:06:24 +0200

    find duplicates and remove them

---
+++ findDuplicates.js
@@ -0,0 +1,18 @@
+const dict = require('./input.json')
+
+// https://stackoverflow.com/questions/2218999/remove-duplicates-from-an-array-of-objects-in-javascript
+newDict = dict.filter((thing, index, self) =>
+  index !== self.findIndex((t) => (
+    t.RailLexicID === thing.RailLexicID 
+)))
+
+console.log(newDict.length)
+
+var fs = require("fs");
+fs.writeFile("./duplicates.json", JSON.stringify(newDict, null, 4), (err) => {
+  if (err) {
+      console.error(err);
+      return;
+  };
+  console.log("File has been created");
+});
